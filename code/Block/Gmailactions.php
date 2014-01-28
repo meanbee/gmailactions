094f78749305d9cdab54213d70bbed9d9d7fd4d2
Ash Smith <ash.smith@meanbee.com>
2014-01-28 16:13:24 +0000

    Finished system config, and custom action name/description for each type: order, creditmemo, shipment. Only works with SMTP, going from and to the same gmail account

--- code/Block/Gmailactions.php
+++ code/Block/Gmailactions.php
@@ -1,11 +1,14 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: ashsmith
- * Date: 27/01/2014
- * Time: 17:22
- */
 
 class Meanbee_Gmailactions_Block_Gmailactions extends Mage_Core_Block_Template {
 
+    public function getViewOrderUrl($order) {
+        return Mage::getUrl("sales/order/view",array('order_id' => $order->getId(), '_secure' => true));
+    }
+    public function getViewShipmentUrl($order) {
+        return Mage::getUrl("sales/order/shipment",array('order_id' => $order->getId(), '_secure' => true));
+    }
+    public function getViewCreditmemoUrl($order) {
+        return Mage::getUrl("sales/order/creditmemo",array('order_id' => $order->getId(), '_secure' => true));
+    }
 } 

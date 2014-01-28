a131bc1c735b691faf0cab7ac927675768088948
Ash Smith <ash.smith@meanbee.com>
2014-01-28 10:38:39 +0000

    Inital Commit of Gmail Action

---
+++ code/Block/Gmailactions.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ashsmith
+ * Date: 27/01/2014
+ * Time: 17:22
+ */
+
+class Meanbee_Gmailactions_Block_Gmailactions extends Mage_Core_Block_Template {
+
+} 
